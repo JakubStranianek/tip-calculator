b1d05b0b9efb19073f9c384cd9c048b53cb651a2
JakubStranianek <j.stranianek@gmail.com>
2022-01-20 14:07:17 +0100

    its done 😇

--- script.js
+++ script.js
@@ -15,3 +15,38 @@ document.getElementById("tbPeople").addEventListener("input", function() {
         borderInput.style.border = "none";
     }
 })
+
+var valuePercent;
+
+function getVal(value) {
+    valuePercent = value;  
+    //alert(valuePercent);  
+}
+
+function calculate() {
+    var valueOfTbPeople = document.getElementById("tbPeople").value;
+    var valueOfTbAmount = document.getElementById("tbAmount").value;
+    var tipAmount;
+    var total;
+
+    if (valueOfTbPeople <= 0 || valueOfTbAmount <= 0) {
+        tipAmount = 0;
+        total = 0;   
+    } else {
+        tipAmount = (valueOfTbAmount) * (valuePercent/100);
+        tipAmount = (tipAmount) / (valueOfTbPeople); 
+        total = ((valueOfTbAmount) / (valueOfTbPeople)) + (tipAmount);
+    }
+
+    document.getElementById("firstMount").innerHTML = "$" + tipAmount.toFixed(2);
+    document.getElementById("secondMount").innerHTML = "$" + total.toFixed(2);
+}
+
+function reset() {
+    var zero = 0;
+    var valueOfTbPeople = document.getElementById("tbPeople").value = "";
+    var valueOfTbAmount = document.getElementById("tbAmount").value = "";
+    document.getElementById("firstMount").innerHTML = "$" + zero.toFixed(2);
+    document.getElementById("secondMount").innerHTML = "$" + zero.toFixed(2);
+
+}
